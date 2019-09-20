5339542a71e2b6e20143df76e8b216f0ffc171fc
Koenig <lak17007@ad.uconn.edu>
2019-09-20 15:59:08 -0400

    Commit initial analysis script

--- Analyze_denit_data.R
+++ Analyze_denit_data.R
@@ -10,26 +10,59 @@ library(rnoaa)     # r package for interfacing with NCDC climate datasets (need 
 library(nlme)      # linear mixed effects models
 library(lsmeans)   # posthoc multiple comparisons on linear mixed effects models
 
-## --------------- Read in raw data --------------- ##
+
+## ------------------------------ Read in raw data ----------------------------- ##
 
 data <- read.csv("./data/LISS.KG.MasterMatrix.7.24.csv",header=TRUE,stringsAsFactors = FALSE)
+str(data)
+data$Month.N <- as.factor(data$Month.N)
+data$VegZone <- as.factor(data$VegZone)
+data$Site <- as.factor(data$Site)
+
+cols <- c("Month.N","VegZone","Site")
+data[cols] <- sapply(data[cols],as.factor)
+
 veg.dat <- read.csv("./data/BIWMA_VegetationSurvey.csv",header=TRUE,stringsAsFactors = FALSE)
 
 
 
-## --------------- Inspect raw data --------------- ##
+## ------------------------------ Inspect raw data ----------------------------- ##
 
 # How many sites? Should be samples from 3 different vegetation zones on each of 5 days (n=15):
 length(unique(data$Site))
 
 
-## --------------- Repeated measures ANOVA --------------- ##
+## --------------- Repeated measures ANOVA on covariate datasets --------------- ##
+
+# 1. denitrification rate 
+  denit.data <- data[which(!is.na(data$Denit.Rate)),]
+  denit=lme(Denit.Rate ~ Month.N*VegZone, data=denit.data, random=~1|Site, method="ML")
+  anova(denit) 
+  ## denitrification rate varies by month, but not by vegetation zone
+  
+  ## pairwise comparisons by month (averaged over vegetation zone, since veg. zone was not significant):
+  posthocsm=lsmeans(denit, pairwise~Month.N, adjust="tukey")   
+  summary(posthocsm)
+  emmeans(denit,pairwise ~ Month.N)
+  emmip(denit, VegZone ~ Month.N)
+  ## denitrification rate differs between May-June; May-July; May-October (denitrification rate was significantly greater in May compared to June, July, and October)
+  
+  
+  emmeans(denit, pairwise ~ Month.N)
+  
+  res.sm<-residuals(soilmoist)
+  qqnorm(res.sm)
+  hist(res.sm)
+  shapiro.test(res.sm)
+
+
+## --------------- Repeated measures ANOVA on covariate datasets --------------- ##
 
 # 1. soil moisture
 
   soilmoist=lme(Soil.Moist ~ Month.N*VegZone, data=data, random=~1|Site, method="ML")
   anova(soilmoist)
-  posthocsm=lsmeans(soilmoist, pairwise~Month.N, adjust="tukey")
+  posthocsm=lsmeans(soilmoist, pairwise~Month.N, adjust="tukey")   # pairwise comparisons grouped by vegetation zone, since there was no significant relationship between vegetation zone and denitrification
   summary(posthocsm)
   
   res.sm<-residuals(soilmoist)
@@ -40,39 +73,32 @@ length(unique(data$Site))
 
 # 2. ammonium
 
-  nh4=lme(nh4 ~ Month.N*VegZone, data=Dataframe, random=~1|Site, method="ML")
+  nh4=lme(nh4 ~ Month.N*VegZone, data=data, random=~1|Site, method="ML")
   anova(nh4)
   posthoc.nh4=lsmeans(nh4, pairwise~Month.N, adjust="tukey")        
   summary(posthoc.nh4)
+  summary(posthoc.nh4$contrasts)[summary(posthoc.nh4$contrasts)$p.value < 0.05,]
   res.nh4<-residuals(nh4)
   qqnorm(res.nh4)
   hist(res.nh4)
   shapiro.test(res.nh4)
   
   
-  log.nh4=lme(log.nh4 ~ Month.N*VegZone, data=Dataframe, random=~1|Site, method="ML")
-  anova(log.nh4)
-  ph.nh4.log=lsmeans(log.nh4, pairwise~Month.N, adjust="tukey")
-  summary(ph.nh4.log)
-  res.ph.nh4.log<-residuals(log.nh4)
-  qqnorm(res.ph.nh4.log)
-  hist(res.ph.nh4.log)
-  shapiro.test(res.ph.nh4.log)
 
 
 
 
 
+# regressions by group:
+  
+  byveg = try3 %>% 
+          group_by("Month.N") %>%
+          do(fit = lm(Denit.Rate ~ Percent_Bare,data= .))
 
 
 
 
-
-
-
-
-
-
+  emmip(noise.lm, type ~ size | side)
 
 
 
